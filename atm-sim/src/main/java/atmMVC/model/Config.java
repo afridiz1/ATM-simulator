30d9396cae1a45cfe6e5e510c4f2741d07c978f7
Zara Afridi <zara,afridi@mail.utoronto.ca>
2020-01-05 18:49:48 -0500

    init

---
+++ atm-sim/src/main/java/atmMVC/model/Config.java
@@ -0,0 +1,14 @@
+package atmMVC.model;
+
+
+public class Config {
+    static String customerFile = "phase2/src/files/customers.txt";
+    static String workerFile = "phase2/src/files/workers.txt";
+    static String accountFile = "phase2/src/files/accounts.txt";
+    static String trxFile = "phase2/src/files/transactions.txt";
+    static String billPaymentFile = "phase2/src/files/outgoing.txt";
+    static String accountRequestFile = "phase2/src/files/accountRequest.txt";
+    static String alertFile = "phase2/src/files/alerts.txt";
+    static String depositFile = "phase2/src/files/deposits.txt";
+    static String appointmentFile = "phase2/src/files/appointments.txt";
+}
